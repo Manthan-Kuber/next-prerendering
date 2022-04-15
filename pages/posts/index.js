518479695fe8beeda1e59b28fab64e7e9890922e
Manthan Kuber <manthankuber873@gmail.com>
2022-04-15 21:53:50 +0530

    Static Site Generation (SSG) with Dynamic Paramters

---
+++ pages/posts/index.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+function PostList({ posts }) {
+  return (
+    <>
+      <h1>Lists of posts</h1>
+      {posts.map((post) => {
+        return (
+          <div key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+              <h2>
+                {post.id} {post.title}
+              </h2>
+            </Link>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default PostList;
+
+//Pre rendered the fetched data using getstatic props
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+
+  return {
+    props: {
+      posts: data.slice(0, 3),
+    },
+  };
+}
