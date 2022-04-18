a24769ae89d62f77fedf69f87ac11035e13e71fc
Manthan Kuber <manthankuber873@gmail.com>
2022-04-18 17:54:50 +0530

    Inspecting SSR Builds

--- pages/news/[category].js
+++ pages/news/[category].js
@@ -20,13 +20,16 @@ function ArticleListByCategory({ articles, category }) {
 export default ArticleListByCategory;
 
 export async function getServerSideProps(context) {
-  const { params } = context;
+  const { params, req, res, query } = context; //Http Request and Response Object
+  console.log(query);
+  console.log(req.headers.cookie);
+  res.setHeader("Set-Cookie", ["name=Munty"]); //Can set a User Id here like we do in Express
   const { category } = params;
   const response = await fetch(
     `http://localhost:4000/news?category=${category}`
   );
   const data = await response.json();
-
+  console.log(`Pre Rendering News Articles for category ${category}`);
   return {
     props: {
       articles: data,
